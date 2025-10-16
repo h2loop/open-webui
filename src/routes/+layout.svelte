64b52b8e53a41b8dcf044d872f5f44bf17039f17
Dinesh Bala <baladnes@gmail.com>
2025-10-16 10:45:20 +0545

    refactor: update references from Open WebUI to H2Loop Playground

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -441,7 +441,7 @@
 			if (type === 'message') {
 				if ($isLastActiveTab) {
 					if ($settings?.notificationEnabled ?? false) {
-						new Notification(`${data?.user?.name} (#${event?.channel?.name}) • Open WebUI`, {
+						new Notification(`${data?.user?.name} (#${event?.channel?.name}) • H2Loop Playground`, {
 							body: data?.content,
 							icon: data?.user?.profile_image_url ?? `${WEBUI_BASE_URL}/static/favicon.png`
 						});
